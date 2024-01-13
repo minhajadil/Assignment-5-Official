acd1215da3ee984f157f24537436517ac8be12c4
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-01-13 17:24:10 +0600

    Update settings.py

--- Bookshop/settings.py
+++ Bookshop/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://book-shop-m3h9.onrender.com','https://*.0.0.0.0']
+CSRF_TRUSTED_ORIGINS = ['https://book-shop-m3h9.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
